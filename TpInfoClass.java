daad1d073e4d54eb77efac4d6a4844c68d0ca737
maryam <maryam.echadi@gmail.com>
2021-12-06 14:48:06 +0100

    third commit

---
+++ TpInfoClass.java
@@ -0,0 +1,9 @@
+
+public class TpInfoClass {
+	public static void main(String args[]) throws Exception{
+		System.out.println("Hello Word");
+		System.out.println("Here we test push to github");
+		System.out.println("Here we test 2 commit");
+	}
+	
+}
